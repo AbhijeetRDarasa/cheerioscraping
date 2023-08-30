ecc8e3134543613cceb22f922ef2e48b8a0897c5
AbhiGitHubRepo <131143440+AbhijeetRDarasa@users.noreply.github.com>
2023-08-30 11:12:53 +0400

    data srapping and tele messaging

---
+++ constants.js
@@ -0,0 +1,17 @@
+let TEXT = 'Chào mừng bạn đến với 11bet.fun, dịch vụ cá cược trực tuyến hàng đầu với hàng trăm sản phẩm cược hấp dẫn \n'
+TEXT = TEXT + '<a href="#">/xs </a>(kiểm tra kết quả sổ số ba miền) \n'
+TEXT = TEXT + '<a href="#">/xsmn </a>ngày-tháng-năm (kiểm tra kết quả sổ số miền nam ngày bất kì) \n'
+TEXT = TEXT + '<a href="#">/xsmb </a>ngày-tháng-năm (kiểm tra kết quả sổ số miền bắc ngày bất kì) \n'
+TEXT = TEXT + '<a href="#">/xsmt </a>ngày-tháng-năm (kiểm tra kết quả sổ số miền trung ngày bất kì) \n'
+
+let XS = 'Vui lòng chọn vùng bạn muốn xem kết quả \n \n'
+XS = XS + '<a href="#"><a href="/xsmb">Miền Bắc</a></a>\n'
+XS = XS + '<span class="tg-spoiler" style="background-color:Gainsboro"><a href="/xsmn">Miền Nam</a></span> \n'
+XS = XS + '<span class="tg-spoiler" style="background-color:Gainsboro"><a href="/xsmt">Miền Trung</a></span>\n'
+XS = XS + '<span class="tg-spoiler" style="background-color:Gainsboro"><a href="#">Huy</a></span>\n'
+
+
+module.exports = {
+    TEXT,
+    XS
+};
